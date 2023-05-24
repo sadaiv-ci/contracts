025084ea68dd076933b26b76c20492db045d8934
aditipolkam <aditipolkam@gmail.com>
2023-05-24 11:27:40 +0530

    feat: :zap: add onlyOwner modifier for adding providers

--- contracts/SadaivBackup.sol
+++ contracts/SadaivBackup.sol
@@ -32,42 +32,36 @@ contract SadaivBackup {
   }
 
   struct Repository {
-    // uint256 id;
+    uint256 id;
     string name;
     string fullname;
     string description;
     uint256 ownerGithubId;
     uint256 size;
+    bool forked;
     string defaultBranch;
     string timestamp;
     string[] topics;
+    string[] languages;
   }
 
   mapping(uint256 => Repository) public userRepos; //repoId => repo
-  mapping(uint256 => mapping(bytes32 => Build)) public repoBuilds; //repoId => barnch+commitHash => build
+  mapping(uint256 => mapping(bytes32 => Build)) public repoBuilds; //repoId => hash(barnch+commitHash) => build
 
-  event NewBuild(
-    Repository repository,
-    Build build,
-    uint256 repoId,
-    string commitHash
-  );
+  event NewBuild(Repository repository, Build build);
 
   function createNewBuild(
     Repository memory _repo,
-    Build memory _build,
-    uint256 _repoId,
-    string memory _commitHash
+    Build memory _build
   ) public onlyOwner {
-    userRepos[_repoId] = _repo;
-
-    repoBuilds[_repoId][
+    userRepos[_repo.id] = _repo;
+    repoBuilds[_repo.id][
       keccak256(
         abi.encodePacked(
           string(abi.encodePacked(_build.branch, _build.commitHash))
         )
       )
     ] = _build;
-    emit NewBuild(_repo, _build, _repoId, _commitHash);
+    emit NewBuild(_repo, _build);
   }
 }
--- contracts/SadaivId.sol
+++ contracts/SadaivId.sol
@@ -11,19 +11,34 @@ interface VerifySignatureInterface {
 }
 
 contract SadaivId {
+  mapping(address => bool) public owners;
   VerifySignatureInterface public verifySignatureContract;
   using Counters for Counters.Counter;
   Counters.Counter public sadaivId;
 
   constructor(address verifyContractAddress) {
+    owners[msg.sender] = true;
     verifySignatureContract = VerifySignatureInterface(verifyContractAddress);
+  }
+
+  modifier onlyOwner() {
+    require(owners[msg.sender], "Not authorized!");
+    _;
+  }
+
+  function makeOwner(address _address) public onlyOwner {
+    owners[_address] = true;
+  }
+
+  function checkIfOwner(address _address) public view returns (bool) {
+    return owners[_address];
   }
 
   mapping(uint256 => mapping(uint256 => bool)) public sadaivIdToGithubId;
   mapping(address => uint256) public SCWAddressToSadaivId;
   mapping(uint256 => string) public contributorData; //sadaiv id to contributor data cid
 
-  event NewUser(uint256 githubId, uint256 sadaivId, address scwAddress);
+  event NewUser(uint256 sadaivId, address scwAddress);
   event NewProfileChange(uint256 sadaivId, string cid);
   event NewProvider(uint256 sadaivId, uint256 provider);
 
@@ -31,28 +46,26 @@ contract SadaivId {
   function registerUser(
     string memory message,
     bytes memory signature,
-    address signer,
-    uint256 githubId
-  ) public {
+    address signer
+  ) public returns (uint256) {
     require(
       verifySignatureContract.verifySignature(signer, message, signature),
       "Address not authorized!"
     );
-
     require(SCWAddressToSadaivId[signer] == 0, "Address already registered.");
     sadaivId.increment();
-    sadaivIdToGithubId[sadaivId.current()][githubId] = true;
     SCWAddressToSadaivId[signer] = sadaivId.current();
-    emit NewUser(githubId, sadaivId.current(), signer);
+    emit NewUser(sadaivId.current(), signer);
+    return sadaivId.current();
   }
 
-  //function to add new github account ids for a single sadaiv userid
+  //function to add github account ids for a sadaiv userid
   function addProviders(
     string memory message,
     bytes memory signature,
     address signer,
     uint256 _newId
-  ) public {
+  ) public onlyOwner {
     require(
       verifySignatureContract.verifySignature(signer, message, signature),
       "Address not authorized!"
@@ -77,8 +90,8 @@ contract SadaivId {
       verifySignatureContract.verifySignature(signer, message, signature),
       "Address not authorized!"
     );
-    uint256 id = SCWAddressToSadaivId[signer];
-    contributorData[id] = cid;
-    emit NewProfileChange(id, cid);
+    uint256 _sadaivId = SCWAddressToSadaivId[signer];
+    contributorData[_sadaivId] = cid;
+    emit NewProfileChange(_sadaivId, cid);
   }
 }
